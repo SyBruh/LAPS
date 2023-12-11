86aab797ef1ae34281cc75345a3b462da303f251
watari1018 <watari1018@163.com>
2023-12-11 15:17:11 +0800

    'U'

--- src/main/java/team8/laps/javaca/model/User.java
+++ src/main/java/team8/laps/javaca/model/User.java
@@ -1,5 +1,61 @@
 package team8.laps.javaca.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class User {
 
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO) 
+private int id; 
+private String user_name; 
+private String password; 
+@ManyToOne
+private Role role;
+public User() {
+
+} 
+public User(String user_name, String password) {
+this.user_name=user_name;
+this.password=password;
 }
+
+
+public String getPassword() {
+return password;
+}
+
+public void setPassword(String password) {
+this.password = password;
+}
+
+public String getUser_name() {
+return user_name;
+}
+
+public void setUser_name(String user_name) {
+this.user_name = user_name;
+}
+
+public int getId() {
+return id;
+}
+
+public void setId(int id) {
+this.id = id;
+}
+
+public Role getRole() {
+return role;
+}
+
+public void setRole(Role role) {
+this.role = role;
+} 
+}
+//as 
+
