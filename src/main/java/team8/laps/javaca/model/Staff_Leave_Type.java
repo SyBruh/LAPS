99e436bce7eddd40e3042d5f0a1fc9bc0ee1bb53
dy-zhou <your_email>
2023-12-11 15:07:58 +0800

    'model'

--- src/main/java/team8/laps/javaca/model/Staff_Leave_Type.java
+++ src/main/java/team8/laps/javaca/model/Staff_Leave_Type.java
@@ -1,5 +1,73 @@
 package team8.laps.javaca.model;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Table(name="StaffLeaveType")
 public class Staff_Leave_Type {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int Id;
+	private int leave_entitle;
+	private int leave_balance;
+	
+	//mapping to table "Staff"
+	@ManyToOne
+	@JoinColumn(name="staff_id")
+	private Staff staff;
+	//mapping to table "Leave_Type"
+	@ManyToOne
+	@JoinColumn(name="LeaveType_id")
+	private Leave_Type leavetype;
+	
+	
+	//Empty Constructor
+	public Staff_Leave_Type() {
+		
+	}
+	//Constructor in case
+	public Staff_Leave_Type(int leave_entitle,int leave_balance) {
+		this.setLeave_balance(leave_balance);
+		this.setLeave_entitle(leave_entitle);
+	}
+	
+	
+	//Getter and Setter
+	public int getLeave_entitle() {
+		return leave_entitle;
+	}
+	public void setLeave_entitle(int leave_entitle) {
+		this.leave_entitle = leave_entitle;
+	}
+	public int getLeave_balance() {
+		return leave_balance;
+	}
+	public void setLeave_balance(int leave_balance) {
+		this.leave_balance = leave_balance;
+	}
+	public Staff getStaff() {
+		return staff;
+	}
+	public void setStaff(Staff staff) {
+		this.staff = staff;
+	}
+	public Leave_Type getLeavetype() {
+		return leavetype;
+	}
+	public void setLeavetype(Leave_Type leavetype) {
+		this.leavetype = leavetype;
+	}
+	
 
 }
