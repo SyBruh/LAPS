80096d38bba518bf71608aa45a9f56320e2f2195
watari1018 <watari1018@163.com>
2023-12-17 15:26:30 +0800

    '1'

--- src/main/java/team8/laps/javaca/controller/AdminController.java
+++ src/main/java/team8/laps/javaca/controller/AdminController.java
@@ -1,5 +1,18 @@
 package team8.laps.javaca.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("Admin")
 public class AdminController {
 
+	
+
+	 @GetMapping("/Admin/updateEntitle")
+	 public String UpdateEntitle(Model model) { 
+	 return "displayCountries";
+	 }
 }
